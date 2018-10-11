76612bc2d3502e20d777c6e99fcb65b63201dc55
ranjennaidu21 <ranjennaidu21@gmail.com>
2018-10-11 21:45:14 +0800

    added data-filter

---
+++ mybookstore-uiedited/src/main/resources/sql-scripts/scripts.sql
@@ -0,0 +1,17 @@
+--check for all books with category 'Arts & Literature' , change it to Arts based on it
+
+UPDATE bookstoredatabasenew.book
+SET category = 'Arts'
+WHERE id = 1;
+
+UPDATE bookstoredatabasenew.book
+SET category = 'Arts'
+WHERE id = 2;
+
+UPDATE bookstoredatabasenew.book
+SET category = 'Arts'
+WHERE id = 3;
+
+UPDATE bookstoredatabasenew.book
+SET category = 'Arts'
+WHERE id = 5;
